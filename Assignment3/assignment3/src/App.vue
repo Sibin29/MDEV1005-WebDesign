4e29092cefc256db58f7fd855e15f15b77e21d99
Sibin29 <Sibin292000>
2025-03-24 23:02:43 -0400

    assignment3

---
+++ Assignment3/assignment3/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/posts">Posts</router-link>
+    <router-link to="/about">About</router-link>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
